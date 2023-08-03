5920d8cec608971cf33b45ed761fd8cca3f0b809
Nothing bro ✓ <125857863+Singhsawan@users.noreply.github.com>
2023-08-03 22:19:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -52,8 +52,8 @@ TMP_DOWNLOAD_DIRECTORY = environ.get("TMP_DOWNLOAD_DIRECTORY", "./DOWNLOADS/")
 COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "/")
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Rajappan")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://escapematrix2h:sawan09h@cluster0.blsyi8a.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Raj")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 #Downloader
